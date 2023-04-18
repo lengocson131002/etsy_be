65725e7eb92ff50f4c7bc47c63a258c7bb3236c3
lengocson <leson131002@gmail.com>
2023-04-19 00:48:11 +0700

    fix cors

--- src/main/java/com/app/commerce/config/SimpleCorsFilter.java
+++ src/main/java/com/app/commerce/config/SimpleCorsFilter.java
@@ -1,5 +1,9 @@
 package com.app.commerce.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
